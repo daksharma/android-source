7c20c417f1db6648be3936aa8e6d63659d43ddcb
Daksh Sharma <dashatrix@gmail.com>
2016-01-04 13:02:42 -0800

    CheckPoint 31 Completed: Java Threads

--- foundation/31-threads/src/com/bloc/threads/ImageGetter.java
+++ foundation/31-threads/src/com/bloc/threads/ImageGetter.java
@@ -1,7 +1,14 @@
 package com.bloc.threads;
 
+import java.net.URL;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.BufferedImage;
+
 public class ImageGetter extends Thread {
 
+	URL mUrl;
+	boolean mOpenWhenCompleted;
 	/*
 	 * ImageGetter
 	 *
@@ -18,6 +25,16 @@ public class ImageGetter extends Thread {
  	 *	The variables passed into it must impact the `run()` method's
  	 *	behavior as described above.
 	/************************************************/
+	public ImageGetter (String url, boolean open) {
+		try {
+			this.mUrl = new URL(url);
+		} catch (Exception e) {
+			System.out.println("URL Invalid");
+			System.out.println(e);
+		}
+		this.mOpenWhenCompleted = open;
+
+	}
 
 	@Override
 	public void run() {
@@ -26,5 +43,27 @@ public class ImageGetter extends Thread {
  		 *	Perform the work found in `Main.java` in this
  		 *	Thread instead.
 		/************************************************/
+
+		try {
+			File existingImage = new File("google_logo.png");
+			if (existingImage.exists()) {
+				existingImage.delete();
+			}
+			URL url = new URL("https://www.google.com/images/srpr/logo11w.png");
+			BufferedImage bufferedImage = ImageIO.read(url);
+			File outputfile = new File("google_logo.png");
+			ImageIO.write(bufferedImage, "png", outputfile);
+			if ("/".equals(System.getProperties().getProperty("file.separator"))) {
+				Runtime.getRuntime().exec("open google_logo.png");
+			} else {
+				Runtime.getRuntime().exec("google_logo.png");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 }
